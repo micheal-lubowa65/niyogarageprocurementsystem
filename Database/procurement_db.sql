288e77496268fd4c13656ca4fbf884d6d23cd356
micheal-lubowa65 <lubowa65@gmail.com>
2025-01-14 14:21:25 +0300

    third commit

--- Database/procurement_db.sql
+++ Database/procurement_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2025 at 08:34 AM
+-- Generation Time: Jan 14, 2025 at 12:19 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -39,17 +39,32 @@ CREATE TABLE `procurement_requests` (
   `contact` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
   `delivery_due_date` date DEFAULT NULL,
-  `req_status` varchar(100) DEFAULT 'pending'
+  `req_status` varchar(100) DEFAULT 'pending',
+  `purchase_order` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `procurement_requests`
 --
 
-INSERT INTO `procurement_requests` (`id`, `item_name`, `item_description`, `quantity`, `justification`, `request_date`, `first_name`, `second_name`, `contact`, `email`, `delivery_due_date`, `req_status`) VALUES
-(20, 'Service Cards', '50 x 50 cm pieces', 200, 'Service Cards for cars', '2025-01-13 19:48:49', 'Maureen', 'Siima', '0774321274', 'maureen.siima@niyogarage.com', '2025-01-16', 'approved'),
-(22, 'External Drive', '500GB SSD TOSHIBA ', 1, 'Needed for extra back up', '2025-01-14 10:12:08', 'Mudali', 'Derrick', '0708195622', 'mudali@gmail.com', '2025-01-23', 'approved'),
-(23, 'Mechanical Book', 'A hand book for Mechanics to support trainings', 1, 'This book has several safety features that can be used in the garage', '2025-01-14 10:17:28', 'Kenneth ', 'Kasumba', '0782059164', 'kenneth.kasumba@gmail.com', '2025-01-30', 'pending');
+INSERT INTO `procurement_requests` (`id`, `item_name`, `item_description`, `quantity`, `justification`, `request_date`, `first_name`, `second_name`, `contact`, `email`, `delivery_due_date`, `req_status`, `purchase_order`) VALUES
+(20, 'Service Cards', '50 x 50 cm pieces', 200, 'Service Cards for cars', '2025-01-13 19:48:49', 'Maureen', 'Siima', '0774321274', 'maureen.siima@niyogarage.com', '2025-01-16', 'approved', 1),
+(22, 'External Drive', '500GB SSD TOSHIBA ', 1, 'Needed for extra back up', '2025-01-14 10:12:08', 'Mudali', 'Derrick', '0708195622', 'mudali@gmail.com', '2025-01-23', 'approved', 2),
+(23, 'Mechanical Book', 'A hand book for Mechanics to support trainings', 1, 'This book has several safety features that can be used in the garage', '2025-01-14 10:17:28', 'Kenneth ', 'Kasumba', '0782059164', 'kenneth.kasumba@gmail.com', '2025-01-30', 'approved', 3);
+
+--
+-- Triggers `procurement_requests`
+--
+DELIMITER $$
+CREATE TRIGGER `update_purchase_order` BEFORE UPDATE ON `procurement_requests` FOR EACH ROW BEGIN
+    
+    IF OLD.req_status = 'pending' AND NEW.req_status = 'approved' THEN
+        
+        SET NEW.purchase_order = (SELECT COALESCE(MAX(purchase_order), 0) + 1 FROM procurement_requests);
+    END IF;
+END
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
@@ -69,7 +84,7 @@ ALTER TABLE `procurement_requests`
 -- AUTO_INCREMENT for table `procurement_requests`
 --
 ALTER TABLE `procurement_requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
